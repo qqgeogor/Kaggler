a6cedbea392055c6af05988b191b051d923900b8
qianqian <qianqian@moseeker.com>
2016-09-08 14:52:01 +0800

    add ftrl fm cython

--- demo/ftrl_fm_cython.py
+++ demo/ftrl_fm_cython.py
@@ -4,21 +4,36 @@ import random
 from math import log
 import numpy as np
 from datetime import datetime
+import pandas as pd
+from sklearn.cross_validation import KFold
+from sklearn.metrics import roc_auc_score
 #### RANDOM SEED ####
-random.seed(5)  # seed random variable for reproducibility
+seed = 1024
+np.random.seed(seed)
 #####################
 
 ####################
 #### PARAMETERS ####
 ####################
-reportFrequency = 1000000
+
+reportFrequency = 1000
 path = "E:\\Redhat\\"
 trainingFile = "E:\\Redhat\\train_le_date.csv"
 testingFile = "E:\\Redhat\\test_le_date.csv"
+train = pd.read_csv(trainingFile)
+test = pd.read_csv(testingFile)
+y = train['outcome'].values
+skf = KFold(len(y), n_folds=4, shuffle=False, random_state=seed)
+for ind_tr, ind_te in skf:
+    X_train = train.iloc[ind_tr]
+    X_test = train.iloc[ind_te]
+    break
+
+X_train.to_csv(path+'X_train.csv',index=False)
+X_test.to_csv(path+'X_test.csv',index=False)
 
 fm_dim = 32
 fm_initDev = .01
-hashSalt = "salty"
 
 alpha = 0.1
 beta = 1.
@@ -29,12 +44,12 @@ beta_fm = 1.
 p_D = 22
 D = 2 ** p_D
 
-L1 = 1.0
-L2 = .1
-L1_fm = 2.0
-L2_fm = 3.0
+L1 = 0.1
+L2 = 1.0
+L1_fm = 0.1
+L2_fm = 1.0
 
-n_epochs = 5
+n_epochs = 3
 
 ####
 start = datetime.now()
@@ -42,7 +57,8 @@ start = datetime.now()
 # initialize a FM learner
 learner = FTRL_FM(fm_dim, fm_initDev, L1, L2, L1_fm, L2_fm, D, alpha, beta, alpha_fm = alpha_fm, beta_fm = beta_fm)
 
-learner.fit(trainingFile=open(trainingFile),n_epochs=5)
+learner.fit(trainingFile=open(path+'X_train.csv'),n_epochs=5,validationFile=open(path+'X_test.csv'),eval_metric=roc_auc_score,reportFrequency=reportFrequency)
+
 # save the weights
 # w_outfile = path+"param.w.txt"
 # w_fm_outfile = path+"param.w_fm.txt"
